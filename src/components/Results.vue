a611e99e5953414272a907aa0e566ae1bc87ef7e
wayne fox <fox.wayne1@gmail.com>
2022-04-21 12:26:08 +0100

    entire timer reaction project

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time = {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  mounted() {
+    if (this.score < 300) {
+      this.rank = 'Holy cow, that was impressive'
+    } else if (this.score < 450) {
+      this.rank = 'that was okay i suppose'
+    } else {
+      this.rank = 'Pretty pitiful'
+    }
+  },
+}
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
